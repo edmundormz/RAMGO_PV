e7b7b1e551f9b53aba5c14378725cbb084b817ce
Edmundo Ramirez <hector.edmundox.ramirez.gomez@intel.com>
2018-02-09 14:19:42 -0600

    insert_producto basic working

--- Lab/mysql_python_101/db_manager.py
+++ Lab/mysql_python_101/db_manager.py
@@ -1,4 +1,5 @@
 import MySQLdb
+import ipdb
 
 
 class DBManager():
@@ -12,10 +13,10 @@ class DBManager():
     def read_all(self):
         pass
 
-    def insert_element(self, **kwargs):
-        insert_producto = "INSERT INTO `Producto` (`Nombre`, `Precio`, `Costo`, `Stock`) VALUES"
+    def insert_producto(self, **kwargs):
+        insert_into = "INSERT INTO `Producto` (`Nombre`, `Precio`, `Costo`, `Stock`) VALUES"
         query = "{operation} ('{nombre}', '{precio}', '{costo}', '{stock}')".format(
-            operation=insert_producto,
+            operation=insert_into,
             nombre=kwargs.get('nombre'),
             precio=kwargs.get('precio'),
             costo=kwargs.get('costo'),
@@ -23,12 +24,14 @@ class DBManager():
         try:
             self.cur.execute(query)
             self.db.commit()
+            print "Product inserted"
         except:
             self.db.rollback()
+            print "Exception"
 
 
 if __name__ == "__main__":
-    DBManager().insert_element(nombre="Polin 4x5",
+    DBManager().insert_producto(nombre="Polin 4x5",
                                precio="125",
                                costo="85",
                                stock="50")
